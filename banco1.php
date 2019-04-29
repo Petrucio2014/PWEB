e3345ac2c821699f8b72ca7f63662022e0b2eb4b
Petrucio2014 <petrucio20111@hotmail.com>
2019-04-29 14:38:33 -0300

    Update banco1.php

--- banco1.php
+++ banco1.php
@@ -53,13 +53,67 @@
 		private int $id;
 		private string $categoria;
 
-		function categoria($id,$categoria){
+		function categoria(int $id, string $categoria){
 			$this->id = $id;
 			$this->categoria = $categoria;
 		}
 	}
 
 	class produto{
-		
+		private int $id;
+		private int $id_categoria;
+		private string $produto;
+		private string $descricao;
+		private string $imagem_produto;
+		private float $preco;
+		private int $quantidade;
+
+		function produto(int $id,int $id_categoria,string $produto,string $descricao,string $imagem_produto,string $preco,string $quantidade){
+			$this->id = $id;
+			$this->id_categoria = $id_categoria;
+			$this->produto = $produto;
+			$this->descricao = $descricao;
+			$this->imagem_produto = $imagem_produto;
+			$this->preco = $preco;
+			$this->quantidade = $quantidade;
+		}
+
 	}
+
+	class pedidoitens{
+		private int $id;
+		private int $id_produto;
+		private int $id_pedido;
+		private string $produto;
+		private int $quantidade;
+		private float $valor;
+		private float $total;
+
+		function pedidoitens(int $id,int $id_produto,int $id_pedido,string $produto,int $quantidade,float $valor,float $total)
+		{
+			$this->id = $id;
+			$this->id_categoria = $id_categoria;
+			$this->produto = $produto;
+			$this->descricao = $descricao;
+			$this->imagem_produto = $imagem_produto;
+			$this->preco = $preco;
+			$this->quantidade = $quantidade;	
+		}
+	}
+
+	class pedido{
+		private int $id;
+		private string $cpf_cliente;
+		private $momento = new DateTime();//da uma olhadinha aqui depois
+		private string $situacao;
+
+		function pedido(int $id, string $cpf_cliente, $momento, string $situacao){
+			$this->id = $id;
+			$this->cpf_cliente = $cpf_cliente;
+			$this->momento = $momento;
+			$this->situacao = $situacao;
+		}
+	}
+
+
 ?>
